0aa5fec902d5c166cd74cd4063894f8072d32618
AlparDiegoNeri <79469209+AlparDiegoNeri@users.noreply.github.com>
2021-08-27 20:38:39 -0300

    Adiciona título
    Adiciona título para as cidades do litoral sul

--- Cidades/Program.cs
+++ Cidades/Program.cs
@@ -6,6 +6,8 @@ namespace Cidades
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Cidades do Litoral Sul de São Paulo:");
+            Console.WriteLine();
             Console.WriteLine("Praia Grande");
             Console.WriteLine("Mongaguá");
             Console.WriteLine("Itanhaém");

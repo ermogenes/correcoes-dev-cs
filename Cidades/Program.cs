be0358f9c816be1e59a5bedfbf128889f8ad062b
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-08-27 20:14:31 -0300

    Adiciona solução do exercício Cidades

---
+++ Cidades/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");
+        }
+    }
+}

1772421fbe19d36341b42d9803da7837b8cdbaeb
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-08-27 21:43:16 -0300

    Adiciona solução do exercício OlaUsuario

---
+++ OlaUsuario/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeUsuario;
+            Console.Write("Digite seu nome: ");
+            nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine($"Olá, {nomeUsuario}!");
+        }
+    }
+}

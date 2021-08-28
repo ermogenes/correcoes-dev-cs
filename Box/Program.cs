56d506094d4ee2491a9b0a60e3c6972cef04ac5b
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-08-27 21:38:33 -0300

    Adiciona solução do exercício Box

---
+++ Box/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-----------+");
+            Console.WriteLine("| Ermogenes |");
+            Console.WriteLine("+-----------+");
+        }
+    }
+}

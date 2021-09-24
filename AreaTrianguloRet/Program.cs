830f8ec4678a4a2f89219cf85db1b51e40f12ab1
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-09-24 20:17:57 -0300

    Adiciona solução do exercício AreaTrianguloRet

---
+++ AreaTrianguloRet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, h, area;
+
+            Console.Write("Base....: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Altura..: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            area = (b * h) / 2;
+
+            Console.WriteLine($"\nÁrea....: {area:N3}");
+        }
+    }
+}

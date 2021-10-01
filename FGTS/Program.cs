ab227880ecebfeff5675b77220c47f74fb6c0d65
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-01 20:22:57 -0300

    Adiciona solução do exercício FGTS

---
+++ FGTS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal salario, fgts;
+            
+            Console.Write("Salário (R$)..: ");
+            salario = Convert.ToDecimal(Console.ReadLine());
+
+            fgts = (salario / 100) * 8;
+
+            Console.WriteLine($"\nFGTS: {fgts:C2}");
+        }
+    }
+}

220755033ac3e7c08229bb9f2b218ba7b622e9cb
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-01 20:08:17 -0300

    Adiciona solução do exercício VelocMedia

---
+++ VelocMedia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distancia, tempo, velocidade;
+
+            Console.Write("Distância percorrida (m): ");
+            distancia = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Tempo gasto (s): ");
+            tempo = Convert.ToDouble(Console.ReadLine());
+
+            velocidade = distancia / tempo;
+
+            Console.WriteLine($"\nVelocidade média: {velocidade:N2} m/s");
+        }
+    }
+}

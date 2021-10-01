eb4ee6cfd419636d178b9b385f5923f3ae09ae03
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-01 19:42:29 -0300

    Adiciona solução do exercício GrausCF

---
+++ GrausCF/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            double tempC = Convert.ToDouble(Console.ReadLine());
+
+            double tempF = tempC * 1.8 + 32;
+
+            Console.WriteLine($"{tempC}°C equivalem a {tempF}°F");
+        }
+    }
+}

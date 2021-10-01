fc7ac4964b2c70f384146fb1e0c850158c3d826b
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-01 19:59:21 -0300

    Adiciona solução do exercício MediaAritmetica

---
+++ MediaAritmetica/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MediaAritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, media;
+
+            Console.WriteLine("Média Aritmética de 3 números\n");
+
+            Console.Write("Digite o primeiro número..: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número...: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o terceiro número..: ");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine($"\nMédia: {media:N1}");
+        }
+    }
+}

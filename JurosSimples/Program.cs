ba833ef3e1e9d2486c6d37ce269015c872812e04
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-01 20:32:44 -0300

    Adiciona solução do exercício JurosSimples

---
+++ JurosSimples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal capital, taxa, meses, juros, montante;
+
+            Console.WriteLine("Juros simples (j)\n");
+
+            Console.Write("Capital [c] (R$).......: ");
+            capital = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Taxa de juros [i] (%)..: ");
+            taxa = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Tempo [t] (meses)......: ");
+            meses = Convert.ToDecimal(Console.ReadLine());
+
+            juros = capital * (taxa / 100) * meses;
+            montante = capital + juros;
+
+            Console.WriteLine($"\nJuros.....: {juros:C2}");
+            Console.WriteLine($"Montante..: {montante:C2}");
+        }
+    }
+}

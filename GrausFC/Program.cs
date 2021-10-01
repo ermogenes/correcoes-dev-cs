ad15c00de42c93e92fa471b09b4989f6f5463538
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-01 19:49:27 -0300

    Adiciona solução do exercício GrausFC

---
+++ GrausFC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a temperatura em graus Fahrenheit: ");
+            double tempF = Convert.ToDouble(Console.ReadLine());
+
+            double tempC = (tempF - 32) / 1.8;
+
+            Console.WriteLine($"{tempF}°F equivalem a {tempC:N2}°C");
+        }
+    }
+}

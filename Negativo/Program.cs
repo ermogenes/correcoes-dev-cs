f4b15c9de11ce56331a99cfad40436a14fc12174
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-29 19:41:00 -0300

    Adiciona solução do exercício Negativo

---
+++ Negativo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("Você digitou um número negativo.");
+            }
+        }
+    }
+}

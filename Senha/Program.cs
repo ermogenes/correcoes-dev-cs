56aa7ffedaa3ae6f69f56bb0f445304edbb8f7d2
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-10-29 21:38:58 -0300

    Adiciona solução do exercício Senha

---
+++ Senha/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SenhaCorreta = "1234abcd";
+            string senha, permissaoAcesso;
+
+            Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+            senha = Console.ReadLine();
+
+            permissaoAcesso = senha == SenhaCorreta ? "permitido" : "negado";
+
+            Console.WriteLine($"Acesso { permissaoAcesso }");
+        }
+    }
+}

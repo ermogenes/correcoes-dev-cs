6f0c846eb7041539efd304b66d4ec21491ad2562
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-11-26 19:41:28 -0300

    Adiciona solução do exercício SequenciaCentena

---
+++ SequenciaCentena/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            while (n <= 100)
+            {
+                Console.Write($"{n} ");
+                n = n + 1;
+            }
+        }
+    }
+}

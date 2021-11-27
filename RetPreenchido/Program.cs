bfaebfe3f5092217c900e17dffe44bdcf7c03cd0
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-11-26 22:30:18 -0300

    Adiciona solução do exercício RetPreenchido

---
+++ RetPreenchido/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RetPreenchido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tamanho do retângulo:");
+
+            Console.Write("Largura..: ");
+            int largura = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Altura...: ");
+            int altura = Convert.ToInt32(Console.ReadLine());
+
+            if (largura < 1 || largura > 10 || altura < 1 || altura > 10)
+            {
+                Console.WriteLine("As medidas devem estar entre 1 e 10.");
+            }
+            else
+            {
+                int linhaAtual = 1;
+                while(linhaAtual <= altura)
+                {
+                    Console.WriteLine();
+
+                    int colunaAtual = 1;
+                    while (colunaAtual <= largura)
+                    {
+                        Console.Write("*");
+                        colunaAtual += 1;
+                    }
+
+                    linhaAtual += 1;
+                }
+            }
+        }
+    }
+}

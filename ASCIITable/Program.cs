595a8889fc9ec24635d4b0bcc5dc22bf8df6745d
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-12-03 19:41:53 -0300

    Adiciona solução do exercício ASCIITable

---
+++ ASCIITable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 32;
+            while (i <= 126)
+            {
+                char caractere = Convert.ToChar(i);
+                Console.Write(caractere);
+                i++;
+            }
+        }
+    }
+}

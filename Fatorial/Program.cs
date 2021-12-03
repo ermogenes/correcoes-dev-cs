903d19c48e64c2bca459b84d2976370cb8bb5d11
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-12-03 20:06:01 -0300

    Adiciona solução do exercício Fatorial

---
+++ Fatorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0 || numero >= 32)
+            {
+                Console.WriteLine("Digite um número natural positivo menor do que 32.");
+            }
+            else
+            {
+                int fatorial = 1;
+
+                if (numero >= 2)
+                {
+                    int i = 2;
+
+                    while(i <= numero)
+                    {
+                        fatorial = fatorial * i;
+                        i += 1;
+                    }
+                }
+
+                Console.WriteLine($"{numero}! = {fatorial}");
+            }
+        }
+    }
+}

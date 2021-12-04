0ea586f1e2c6f364aae1c579ea2ddeedf12cdf4d
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-12-03 22:32:25 -0300

    Adiciona solução do exercício SomaDigitos

---
+++ SomaDigitos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SomaDigitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro positivo: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero <= 0)
+            {
+                Console.WriteLine("Somente números maiores que zero.");
+            }
+            else
+            {
+                Console.Write("\nDígitos: ");
+
+                int somaDigitos = 0;
+
+                while (numero > 0)
+                {
+                    int proximoNumero = numero / 10;
+                    int digitoAtual = numero % 10;
+
+                    somaDigitos += digitoAtual;
+                    numero = proximoNumero;
+
+                    Console.Write($"{digitoAtual} ");
+                }
+
+                Console.WriteLine($"\n\nSoma = {somaDigitos}");
+            }
+        }
+    }
+}

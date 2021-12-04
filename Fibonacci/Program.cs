8e8f568b4df8da267bcf08776cd2638babeb1158
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-12-03 21:50:23 -0300

    Adiciona solução do exercício Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sequência de Fibonacci");
+
+            Console.Write("Quantos termos (>=2)? ");
+            int numeroTermos = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroTermos < 2 || numeroTermos > 47)
+            {
+                Console.WriteLine("Digite um número entre 2 e 47.");
+            }
+            else
+            {
+                int anterior = 0;
+                int atual = 1;
+                Console.Write($"{anterior} {atual} ");
+
+                int i = 3;
+                while(i <= numeroTermos)
+                {
+                    int termo = atual + anterior;
+                    Console.Write($"{termo} ");
+                    
+                    anterior = atual;
+                    atual = termo;
+
+                    i += 1;
+                }
+            }
+        }
+    }
+}

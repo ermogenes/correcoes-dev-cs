ec7e0d7d2b5c7336343b5aa8f6212abebddebc07
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2022-07-25 17:19:47 -0300

    Adiciona soluções de diversos exercícios

---
+++ AmortizacaoConstante/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("--- Tabela SAC ---\n");
+
+Console.Write("Principal (em R$)..: ");
+decimal principal = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Períodos...........: ");
+int periodos = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Taxa (em %)........: ");
+double taxa = Convert.ToDouble(Console.ReadLine());
+
+decimal amortizacao = principal / periodos;
+decimal juros, prestacao, valorPago, saldoDevedor;
+
+int periodo = 0;
+saldoDevedor = principal;
+valorPago = 0;
+
+Console.WriteLine();
+Console.WriteLine($"{"Período",-18} {"Amortização",-18} {"Juros",-18} {"Prestação",-18} {"Valor Pago",-18} {"Saldo Devedor",-18}");
+
+while (periodo <= periodos)
+{
+    if (periodo == 0)
+    {
+        Console.WriteLine($"{periodo,-18:N0} {"",-18} {"",-18} {"",-18} {"",-18} {saldoDevedor,-18:C}");
+    }
+    else
+    {
+        juros = saldoDevedor * Convert.ToDecimal(taxa / 100);
+        prestacao = amortizacao + juros;
+        valorPago += prestacao;
+        saldoDevedor -= amortizacao;
+        Console.WriteLine($"{periodo,-18:N0} {amortizacao,-18:C} {juros,-18:C} {prestacao,-18:C} {valorPago,-18:C} {saldoDevedor,-18:C}");
+    }
+    periodo++;
+}
---
+++ UrnaPlebiscito/Program.cs
@@ -0,0 +1,106 @@
+﻿const string SegredoParaFechamentoDaUrna = "s234f$WR";
+int votosSim = 0, votosNao = 0, abstencoes = 0, totalVotos = 0;
+
+string comandoMesario = "";
+while (comandoMesario != SegredoParaFechamentoDaUrna)
+{
+    // Menu do mesário
+    Console.Clear();
+    Console.WriteLine("--- Urna Eletrônica ---\n");
+
+    Console.Write("Aguarde a intervenção do mesário... ");
+    comandoMesario = Console.ReadLine()!;
+
+    if (comandoMesario.ToUpper() == "P")
+    {
+        // Voto liberado
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("--- Urna Eletrônica ---\n");
+
+            Console.WriteLine("Plebiscito: Você é a favor da proibição do uso de boné em sala de aula?\n");
+
+            Console.WriteLine("    S - Sim, sou a favor");
+            Console.WriteLine("    N - Não, sou contra");
+            Console.WriteLine("    A - Me abstenho de responder");
+
+            Console.Write("\nDigite a opção desejada e pressione [ENTER]: ");
+            string voto = Console.ReadLine()!.Trim().PadLeft(1, ' ').Substring(0, 1).ToUpper();
+
+            string textoVoto = "";
+            // Validação e Contagem
+            switch (voto)
+            {
+                case "S":
+                    votosSim++;
+                    textoVoto = "Sim, sou a favor";
+                    break;
+                case "N":
+                    votosNao++;
+                    textoVoto = "Não, sou contra";
+                    break;
+                case "A":
+                    abstencoes++;
+                    textoVoto = "Me abstenho de responder";
+                    break;
+                default:
+                    // Opção inválida, refaz votação do mesmo eleitor
+                    continue;
+            }
+
+            // Votação válida, segue para o próximo eleitor 
+            totalVotos++;
+            Console.WriteLine($"\nVocê votou \"{textoVoto}\"");
+            Console.Write("\nPressione uma tecla para finalizar sua votação...");
+            Console.ReadKey();
+            break;
+        }
+    }
+}
+
+// Se ninguém votou, finaliza o programa
+if (totalVotos == 0) return;
+
+// Calcula resultado
+double percentualGeralSim = (double)votosSim / totalVotos * 100;
+double percentualGeralNao = (double)votosNao / totalVotos * 100;
+double percentualGeralAbstencoes = (double)abstencoes / totalVotos * 100;
+double votosValidos = votosSim + votosNao;
+double percentualVotosValidos = votosValidos / totalVotos * 100;
+
+string resultado;
+ConsoleColor corExibicao;
+if (votosValidos <= (totalVotos / 2)) // Válidos devem ser maioria simples
+{
+    resultado = "INDETERMINADO";
+    corExibicao = ConsoleColor.Yellow;
+}
+else if (votosSim > votosNao) // Empate é "REPROVADO"
+{
+    resultado = "APROVADO";
+    corExibicao = ConsoleColor.Green;
+}
+else
+{
+    resultado = "REPROVADO";
+    corExibicao = ConsoleColor.Red;
+}
+
+// Exibe resultado
+Console.Clear();
+Console.WriteLine("--- Urna Eletrônica ---\n");
+Console.WriteLine("Plebiscito: Você é a favor da proibição do uso de boné em sala de aula?\n");
+Console.WriteLine($"Finalizado com o total de {totalVotos} voto(s).\n");
+
+Console.WriteLine("Contagem de votos:");
+Console.WriteLine($"S - Sim, sou a favor         - {votosSim} ({percentualGeralSim:N1}%)");
+Console.WriteLine($"N - Não, sou contra          - {votosNao} ({percentualGeralNao:N1}%)");
+Console.WriteLine($"A - Me abstenho de responder - {abstencoes} ({percentualGeralAbstencoes:N1}%)");
+
+Console.WriteLine($"\nVotos válidos: {votosValidos} ({percentualVotosValidos:N1}%)");
+
+Console.Write("\nResultado do plebiscito: ");
+Console.ForegroundColor = corExibicao;
+Console.WriteLine(resultado);
+Console.ResetColor();
